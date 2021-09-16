9db6c0301b395a86c62126a83e2764623b22075b
kaighcds <90645202+kaighcds@users.noreply.github.com>
2021-09-16 18:22:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Welcome to Kai's Bistro!")
+print("please choose your drink")
+
+customer_order = input("Z-Vannila Cappuccino  X-Black Coffee  Y- Iced Latte  W- Vannila Frappuccino")
+
+if customer_order =="Z":
+  print("Here is your Vannila Cappuccino!")
+elif customer_order =="Y":
+  print("enjoy your Iced Latte!")
+elif customer_order =="X":
+   print("Here is your Black Coffee!")
+elif customer_order =="W":
+  print("enjoy your Vannila Frappuccino!")
+else:
+   print("We do not have that try again..") 
